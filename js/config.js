4471e0fcb0bf0e18d423b33be0b63a507ff75999
wusger <59761912+wusger@users.noreply.github.com>
2020-10-14 18:03:59 +0800

    830

---
+++ js/config.js
@@ -0,0 +1,1 @@
+window.config={"logo":"","background":"#C4C4C4","shell":"","showControlPanel":false,"iOSGuide":true,"alwaysShowLinks":false,"hoverShowLinks":false,"hoverShowRemarks":true,"showComments":true,"showQRCode":false,"hintLinks":false,"scrollRemarks":true,"isShowAllPageNode":false,"isShowPreviewTools":false,"showRemarkFlag":false}
